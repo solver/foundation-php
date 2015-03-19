68ebc82a95d2156e5e0f63ac9121a29b587aa5f2
Stan Vass <stanvass@users.noreply.github.com>
2015-03-19 04:29:15 +0200

    A stub for TupleFormat (no class yet) to remind me to add this later.

---
+++ app/classes/Solver/Lab/[Format]/TupleFormat.php
@@ -0,0 +1,8 @@
+<?php
+// TODO: TupleFormat will be a variation of ListFormat, where every index has a different format. 
+//
+// A tuple has a fixed length, but we can tolerate optional trailing tuple values by providing defaults in the format,
+// which means the output is still of fixed length.
+//
+// Usage (shows quadruple with one optional element; optionals are always at the end, like optional function args):
+// $tupleFormat = (new TupleFormat)->add($fmt)->add($fmt)->add($fmt)->addWithDefault(123, $fmt);
