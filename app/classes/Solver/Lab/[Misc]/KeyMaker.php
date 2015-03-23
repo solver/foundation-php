8b11b1be2c31b96b06ccae7996a824ae46a01e72
Stan Vass <stanvass@users.noreply.github.com>
2015-03-24 01:44:05 +0200

    KeyMaker now uses a crypto strong source of randomness by default (the old algorithm is available via a new optional flag).

--- app/classes/Solver/Lab/[Misc]/KeyMaker.php
+++ app/classes/Solver/Lab/[Misc]/KeyMaker.php
@@ -29,19 +29,32 @@ class KeyMaker {
 	 * @param string $alphabet
 	 * Optional (default = null). A string of characters to use for creating the key. If you pass null (or nothing),
 	 * the default alphabet used will be all lowercase and uppercase latin letters plus all digits (total of 62
-	 * characters). The keys produced with the default alphabet can then be included verbatim without processing and
-	 * escaping in a wide range of mediums and encodings (ASCII, UTF-8, 7-bit encoding, URLs, JSON etc.).
+	 * characters). The keys produced with the default alphabet are selected to be usable verbatim without processing 
+	 * and escaping in a wide range of mediums and encodings (ASCII, UTF-8, 7-bit encoding, URLs, JSON etc.).
+	 * 
+	 * @param bool $cryptoWeak
+	 * Default false. Enable this to use a much faster and still sufficiently random, but cryptographically insecure
+	 * algorithm. If you enable this flag and use the resulting keys as tokens, passwords etc. your application may be
+	 * exploitable. 
 	 * 
 	 * @return string
 	 * Random key string.
 	 */
-	public static function getKey($length, $alphabet = null) {
+	public static function getKey($length, $alphabet = null, $cryptoWeak = false) {
 		if ($alphabet === null) $alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
-		
+		$alphabetLength = \strlen($alphabet);
 		$key = '';
 		
-		for ($i = 0; $i < $length; $i++) {
-			$key .= $alphabet[\mt_rand(0, \strlen($alphabet) - 1)];
+		if ($cryptoWeak) {
+			for ($i = 0; $i < $length; $i++) {
+				$key .= $alphabet[\mt_rand(0, $alphabetLength - 1)];
+			}
+		} else {
+			$bytes = CryptUtils::getRandomBytes($length);
+			
+			for ($i = 0; $i < $length; $i++) {
+				$key .= $alphabet[ord($bytes[$i]) % $alphabetLength];
+			}
 		}
 		
 		return $key;
