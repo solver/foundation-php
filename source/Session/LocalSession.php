0074e0e56c74a2af77ebea24728d5f603d8893fa
Stan Vass <stanvass@users.noreply.github.com>
2015-05-03 18:07:31 +0300

    Moved LocalSession to namespace Solver\Session.

--- source/Session/LocalSession.php
+++ source/Session/LocalSession.php
@@ -11,7 +11,7 @@
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
  */
-namespace Solver\Lab;
+namespace Solver\Session;
 
 /**
  * Creates a session-bound array under a random unique key, which can be sent to the client side to identify the piece
